6168b307cd2017e671de92618ad809a3f4aa059e
sahilpatel18 <sahil.41801@gmail.com>
2020-10-31 00:38:54 -0400

    installed react-chartjs-2 and styled line graph

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component{
  render(){
    const {data} = this.state
     return (
-      <div>
+      <div className={styles.container}>
         <Cards data={data} />
         <CountryPicker />
         <Chart />
