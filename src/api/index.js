100e4d1b22312602e22d4d78053aca0229e51a01
sahilpatel18 <sahil.41801@gmail.com>
2020-10-31 01:38:24 -0400

    taking in the selected country as an argument to get the specified countries api data

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,19 @@ import axios from "axios";
 
 const url = `https://covid19.mathdro.id/api`;
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  } else {
+    changeableUrl = url;
+  }
+
   try {
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
-    } = await axios.get(url);
+    } = await axios.get(changeableUrl);
     const modifiedData = {
       confirmed,
       recovered,
@@ -30,5 +38,14 @@ export const fetchDailyData = async () => {
     return modifiedData;
   } catch (err) {
     console.log(err);
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const response = await axios.get(`${url}/countries`);
+    return response.data.countries.map((country) => country.name);
+  } catch (error) {
+    console.log(error);
   }
 };
