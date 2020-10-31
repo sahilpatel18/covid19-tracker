ab4a34b58eec8dd1b813f5a22179e1874895892d
sahilpatel18 <sahil.41801@gmail.com>
2020-10-31 00:40:28 -0400

    using react-chartjs-2 to display api data in a line graph

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,12 +1,43 @@
-
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api/index";
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./Chart.module.css";
 
 const Chart = () => {
-   return (
-       <div>
-           Chart
-       </div>
-   )
-}
+  const [dailyData, setDailyData] = useState([]);
 
-export default Chart
+  useEffect(() => {
+    const fetchApi = async () => {
+      setDailyData(await fetchDailyData());
+    };
+
+    fetchApi();
+  }, []);
+
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255,0,0,0.5)",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  return <div className={styles.container}>{lineChart}</div>;
+};
+
+export default Chart;
